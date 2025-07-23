c4e38c23d9b22bb76b8841eabe14ce80ac5b2500
Berkay Tumal <berkaytml34@gmail.com>
2025-07-24 01:21:16 +0300

    feat: Add early return for scale callback to prevent unnecessary processing

--- src/effects/scale.js
+++ src/effects/scale.js
@@ -3,6 +3,7 @@ export default {
     callback: (element, scale2rScaleX = 1, scaleY = undefined) => {
         const c_scaleX = 1 / scale2rScaleX;
         const c_scaleY = 1 / (scaleY !== undefined ? scaleY : scale2rScaleX);
+        if (c_scaleX == 0 || c_scaleY == 0 || (c_scaleX == 1 && c_scaleY == 1)) return '';
         return `
         <feImage result="gradient" xlink:href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><linearGradient id='g' x1='0' y1='0' x2='1' y2='0'><stop offset='0%' stop-color='rgba(0,0,0,.5)'/><stop offset='100%' stop-color='rgba(255,255,255,.5)'/></linearGradient><rect width='100%' height='100%' fill='url(%23g)'/></svg>" />
         <feImage result="gradient2" xlink:href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><linearGradient id='g' x1='0' y1='0' x2='0' y2='1'><stop offset='0%' stop-color='rgba(0,0,0,.5)'/><stop offset='100%' stop-color='rgba(255,255,255,.5)'/></linearGradient><rect width='100%' height='100%' fill='url(%23g)'/></svg>" />
